63537bc0324333de565c54d7e8c57654ac46ee5c
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-10 21:40:58 +0200

    start building UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "styles/tailwind.css"
+import { GeistSans } from 'geist/font/sans';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${GeistSans.className} h-screen w-full bg-gray-100 dark:bg-gray-900`}>{children}</body>
     </html>
   )
 }
