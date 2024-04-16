2e773c99dbd8222316017655b74b5385429b2b32
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-17 00:37:25 +0200

    refactor: Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import { cva, type VariantProps } from 'class-variance-authority';
-
 import { twMerge } from 'tailwind-merge';
 
 const button = cva(
@@ -12,7 +11,6 @@ const button = cva(
     'border',
     'border-zinc-700',
     'transition-all',
-    'delay-50',
     'active:scale-95',
     'font-semibold',
   ],
@@ -21,13 +19,19 @@ const button = cva(
       variant: {
         primary: ['bg-zinc-900', 'text-zinc-200', 'hover:enabled:bg-zinc-800'],
         secondary: ['bg-zinc-300', 'text-zinc-900', 'hover:enabled:bg-zinc-800', 'hover:enabled:text-zinc-200'],
+        ghost: [
+          'border-none',
+          'bg-transparent',
+          'text-blue-700',
+          'hover:enabled:bg-blue-800/30',
+          'hover:enabled:text-blue-400',
+        ],
       },
       size: {
-        sm: ['min-w-20', 'h-fit', 'min-h-10', 'text-sm', 'py-1.5', 'px-4'],
+        sm: ['min-w-fit', 'h-fit', 'min-h-10', 'text-sm', 'py-1.5', 'px-4'],
         base: ['min-w-24', 'h-fit', 'min-h-10', 'text-base', 'py-2', 'px-4'],
         lg: ['min-w-32', 'h-fit', 'min-h-12', 'text-lg', 'py-2.5', 'px-6'],
       },
-      underline: { true: ['underline'], false: [] },
     },
     defaultVariants: {
       variant: 'primary',
@@ -40,9 +44,9 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
   underline?: boolean;
 }
 
-export default function Button({ className, variant, size, underline, ...props }: ButtonProps) {
+export default function Button({ className, variant, size, ...props }: ButtonProps) {
   return (
-    <button className={twMerge(button({ variant, size, className, underline }))} {...props}>
+    <button className={twMerge(button({ variant, size, className }))} {...props}>
       {props.children}
     </button>
   );
