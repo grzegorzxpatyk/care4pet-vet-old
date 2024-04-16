63e01c1880e3ea739cdbaa6ad17344fcd0eecd57
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-17 00:38:08 +0200

    refactor: CustomerList - turn ul into table element

--- components/CustomersList/CustomersList.tsx
+++ components/CustomersList/CustomersList.tsx
@@ -1,18 +1,39 @@
+import Link from 'next/link';
 import { fetchCustomers } from 'app/lib/data';
+import Button from 'components/Button/Button';
 
 export default async function CustomersList() {
   const customers = await fetchCustomers();
   console.log(customers);
 
   return (
-    <div className='flex flex-col items-start justify-start gap-8'>
-      <ul>
-        {customers.map((customer) => (
-          <li key={customer.id}>
-            {customer.name} {customer.email} {customer.phonenumber}
-          </li>
-        ))}
-      </ul>
+    <div className='flex w-full flex-col items-start justify-start gap-8'>
+      <table className='w-full table-auto'>
+        <thead className='bg-blue-800/30'>
+          <tr>
+            <td className='rounded-s p-2'>Name</td>
+            <td>Email</td>
+            <td>Phone number</td>
+            <td className='w-20 rounded-e'></td>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.name}</td>
+              <td>{customer.email}</td>
+              <td>{customer.phonenumber}</td>
+              <td>
+                <Link href={`/dashboard/customers/${customer.id}`} passHref>
+                  <Button variant={'ghost'} size={'sm'}>
+                    edit
+                  </Button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
